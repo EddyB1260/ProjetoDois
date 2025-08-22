51550d7e2469ba3f5aca213afae33378ae506f3b
eddyhgs <eddyhgs@gmail.com>
2025-08-22 12:41:58 -0300

    várias mudanças

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Registro(models.Model):
+    primeiro_nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    email = models.EmailField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=15)
+
+
+    def __str__(self):
+        return f"{self.primeiro_nome} {self.sobrenome} - {self.cidade}"
+    
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+from .models import Registro
+
 
 # Create your views here.
+def home(request):
+    registros = Registro.objects.all()
+    #Checar pra ver o login
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login realizado com sucesso!")
+            return redirect('home')
+        else:
+            messages.error(request, "Usuário ou senha inválidos.")
+            return render(request, 'home.html', {})
+
+    else:
+     return render(request, 'home.html', {'registros': registros})
+
+
+def base(request):
+    return render(request, 'base.html', {})
+
+def login_user(request):
+    
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logout realizado com sucesso!")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+            messages.success(request, "Usuário registrado com sucesso!")
+            return redirect('home')
+        else:
+            form = SignUpForm()
+            messages.error(request, "Erro ao registrar usuário. Verifique os dados e tente novamente.")
+            return render(request, 'register.html', {'form': form})
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
